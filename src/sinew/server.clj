aee41af7bed8a91a7749acd70359c71af8d3f44a
David Banks <amoebae@gmail.com>
2015-01-24 20:46:28 +0000

    return formatted list in plaintext

--- src/sinew/server.clj
+++ src/sinew/server.clj
@@ -1,11 +1,14 @@
 (ns sinew.server
   (:require [ring.adapter.jetty :as jetty]
+            [clojure.pprint :as pprint]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [sinew.select-by-tag :as select-by-tag]))
 
 (defn render-index []
-  (select-by-tag/query-by-tag "some-tag"))
+  {:headers {"Content-Type" "text/plain"}
+   :body (with-out-str
+           (pprint/pprint (select-by-tag/query-by-tag "some-tag")))})
 
 (defroutes app
   (GET "/" [] (render-index))
