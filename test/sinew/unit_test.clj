a166026cd1c5bde726314f8470d8a64e3f1f0836
David Banks <amoebae@gmail.com>
2018-03-13 14:36:07 +0000

    framework for pick-next-scene happy path test

--- test/sinew/unit_test.clj
+++ test/sinew/unit_test.clj
@@ -1,6 +1,15 @@
 (ns sinew.unit-test
   (:require [clojure.test :refer :all]
+            [sinew.server :as server]
             [sinew.scan-filenames :as scan-filenames]))
+
+
+(deftest pick-next-scene-works
+  (let [configuration nil
+        repository nil
+        watched nil]
+    (let [result (server/pick-next-scene configuration repository watched)]
+      (is result))))
 
 (deftest lowercase-works 
   (let [result (scan-filenames/lowercase "Hello world")]
