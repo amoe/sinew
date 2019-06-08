82db8e2b3ee381b39a7a63d81dd36386d28dbf3f
David Banks <amoebae@gmail.com>
2019-06-08 07:30:37 +0100

    update ring versions

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.postgresql/postgresql "9.4.1208.jre7"]
                  [ragtime "0.6.0"]
@@ -16,15 +16,16 @@
                  [com.gearswithingears/shrubbery "0.4.1"]
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [commons-io "2.4"]
-                 [ring/ring-core "1.3.2"]
-                 [ring/ring-jetty-adapter "1.3.2"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]
                  [compojure "1.5.0"]
                  [prone "0.8.0"]
                  [me.raynes/fs "1.4.6"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/tools.cli "0.3.1"]]
 
-  :plugins [[lein-ring "0.9.1"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler sinew.server/make-app}
 
   :aliases {"migrate"  ["run" "-m" "sinew.db-setup/migrate"]
             "rollback" ["run" "-m" "sinew.db-setup/rollback"]}
